7605d725c5c97b4a6939bbad3dac300d965ea6b1
Shunsuke Tamiya <shunsuke.tamiya@legalforce.co.jp>
2021-10-03 15:50:41 +0900

    Improve test

--- test.rb
+++ test.rb
@@ -4,5 +4,9 @@ require 'securerandom'
 puts FastUUID::VERSION
 uuid = SecureRandom.uuid.upcase
 puts uuid
+
+# valid uuid
 puts FastUUID.uuid?(uuid)
+
+# invalid uuid
 puts FastUUID.uuid?('xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx')
